dc22e8baa1eab34e2f215ba4118167311b4e6fc2
mihailryzij0 <mihailryzij0@gmail.com>
2021-12-01 17:22:07 +0300

    changed http to https

--- src/getCityWeather.js
+++ src/getCityWeather.js
@@ -3,7 +3,7 @@ export async function getCityWeather(lat, lon){
     let url;
     const key = '1a5c5680e133ff4f8f7361b3bff271a3'
     if(typeof lat == "number"){
-         url = `http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&lang=ru&appid=${key}`;
+         url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&lang=ru&appid=${key}`;
     }else{
          url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${lat}&lang=ru&appid=${key}`;
     }
